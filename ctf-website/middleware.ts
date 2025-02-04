1f9af9a31a350dfbbaacd23aaabcc1bbe1d12049
0xKits <kitsgit@gmail.com>
2025-02-04 21:20:14 +0530

    bruh it broke completely brb

--- ctf-website/middleware.ts
+++ ctf-website/middleware.ts
@@ -2,8 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return NextResponse.redirect("/")
-  // return await updateSession(request);
+  return await updateSession(request);
 }
 
 export const config = {

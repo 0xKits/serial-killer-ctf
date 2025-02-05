74185be54f60b7671bd24055fe35cf6c59e5f4d0
0xKits <kitsgit@gmail.com>
2025-02-05 10:40:33 +0530

    added congratulatory page

--- ctf-website/app/(protected-routes)/tipline/page.tsx
+++ ctf-website/app/(protected-routes)/tipline/page.tsx
@@ -90,12 +90,6 @@ export default function CaseFilesLanding() {
 		});
 	}, []);
 
-	// useEffect(() => {
-	// 	supabase.auth.getUser().then((user) => {
-
-	// 	});
-	// });
-
 	function handleSetUsername(username: string) {
 		supabase.auth.getUser().then((user) => {
 			if (user.data.user) {
@@ -165,7 +159,8 @@ export default function CaseFilesLanding() {
 										variant={"secondary"}
 										className=""
 										onClick={() => {
-											supabase.auth.signOut();router.refresh();
+											supabase.auth.signOut();
+											router.refresh();
 										}}
 									>
 										Log Out
@@ -190,52 +185,84 @@ export default function CaseFilesLanding() {
 
 						<div className="p-6">
 							<div className="grid gap-6 md:grid-cols-2">
-								{questions?.map((caseFile) => (
-									<div
-										key={caseFile.id}
-										className={`border h-fit rounded-lg overflow-hidden transition-shadow duration-300 ${
-											expandedCase === caseFile.id
-												? "shadow-lg"
-												: "shadow"
-										}`}
-									>
+								{questions
+									?.sort((a, b) => a.index - b.index)
+									.map((caseFile) => (
 										<div
-											className={`p-4 flex items-center cursor-pointer `}
-											onClick={() =>
-												toggleCase(caseFile.id)
-											}
+											key={caseFile.id}
+											className={`border h-fit rounded-lg overflow-hidden transition-shadow duration-300 ${
+												expandedCase === caseFile.id
+													? "shadow-lg"
+													: "shadow"
+											}`}
 										>
-											{solves?.includes(
-												caseFile.index
-											) && (
-												<CheckCircle className="mr-3 text-green-500" />
-											)}
-											{!solves?.includes(
-												caseFile.index
-											) && (
-												<Folder className="mr-3 text-[#00285e]" />
-											)}
-											<h3 className="text-lg font-semibold flex-grow">
-												{caseFile.title}
-											</h3>
-										</div>
-										{expandedCase === caseFile.id && (
-											<div className="p-4 bg-gray-50 border-t">
-												<p className="text-sm mb-4">
-													{caseFile.description}
-												</p>
-												<Link
-													href={`/tipline/case/${caseFile.index}`}
-													className="inline-block bg-[#b41e22] text-white px-4 py-2 rounded hover:bg-[#8e1518] transition-colors duration-300"
-												>
-													View Case Details
-												</Link>
+											<div
+												className={`p-4 flex items-center cursor-pointer `}
+												onClick={() =>
+													toggleCase(caseFile.id)
+												}
+											>
+												{solves?.includes(
+													caseFile.index
+												) && (
+													<CheckCircle className="mr-3 text-green-500" />
+												)}
+												{!solves?.includes(
+													caseFile.index
+												) && (
+													<Folder className="mr-3 text-[#00285e]" />
+												)}
+												<h3 className="text-lg font-semibold flex-grow">
+													{caseFile.title}
+												</h3>
 											</div>
-										)}
-									</div>
-								))}
+											{expandedCase === caseFile.id && (
+												<div className="p-4 bg-gray-50 border-t">
+													<p className="text-sm mb-4">
+														{caseFile.description}
+													</p>
+													<Link
+														href={`/tipline/case/${caseFile.index}`}
+														className="inline-block bg-[#b41e22] text-white px-4 py-2 rounded hover:bg-[#8e1518] transition-colors duration-300"
+													>
+														View Case Details
+													</Link>
+												</div>
+											)}
+										</div>
+									))}
 							</div>
 						</div>
+						{solves?.includes(8) && (
+							<div className="w-full flex flex-col justify-center items-center p-6 bg-green-100 border-t border-green-200">
+								<h3 className="text-2xl font-bold text-green-700 mb-4">
+									Congratulations, Special Agent!
+								</h3>
+								<p className="text-lg text-center text-green-600 mb-4">
+									The Federal Bureau of Investigation extends
+									its deepest gratitude for your exceptional
+									work on this case. Your dedication and skill
+									have been instrumental in bringing this
+									investigation to a successful conclusion.
+									Due to the exceptional skill you have demonstrated
+									in solving this case, we would like to offer you a position
+									as cybersecurity analyst at the FBI. Reach out to our recruiter, <b>@0xKits</b>, on
+									Discord with the following message: <b>SSN_FB1_CTF_2o25</b>
+									
+								</p>
+								<div className="bg-white p-4 rounded-lg shadow-md border border-green-300">
+									<h4 className="text-xl font-semibold text-[#00285e] mb-2">
+										OFFICIAL ANNOUNCEMENT
+									</h4>
+									<p className="text-gray-700">
+										We are pleased to announce that the notorious serial killer known as 
+										"Viktor Kane Doom" has been apprehended. We extend our sincere thanks to
+										the civilian investigators who were instrumental in gathering the required evidence
+										
+									</p>
+								</div>
+							</div>
+						)}
 					</div>
 				</main>
 
